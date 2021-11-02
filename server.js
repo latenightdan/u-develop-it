74095fe3636a9d3fe75f664af843189092b2f2fd
daniel natale <dnatale667@gmail.com>
2021-11-01 20:10:26 -0400

    connected

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express');
 
+const mysql = require('mysql2');
+
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-
+//Connect to sql database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //YOUR MYSQL USERNAME
+        user: 'root',
+        //your sql password
+        password:'Tw@tson100',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+db.query(`SELECT * FROM candidates`, (err, rows) =>{
+    console.log(rows);
+})
 //DEFAULT FOR (NOT FOUND)
 app.use((req, res) =>{
     res.status(404).end();
