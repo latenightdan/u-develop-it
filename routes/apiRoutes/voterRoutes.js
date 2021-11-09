41ba27c3c65983543d8e256b7e9ed38914c6b9a4
daniel natale <dnatale667@gmail.com>
2021-11-09 12:22:13 -0500

    voters done

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../../db/connection');
 const inputCheck = require('../../utils/inputCheck');
 
+
 router.get('/voters', (req, res) =>{
     const sql = `SELECT * FROM voters ORDER BY last_name`;
 
@@ -59,6 +60,27 @@ router.get('/voter/:id', (req, res) => {
               });
           }
       });
+  });
+
+  router.post('/voter', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'email');
+    if(errors){
+      res.status(400).json( { error: errors});
+      return;
+    }
+    const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.email];
+
+    db.query(sql, params, (err, result) => {
+      if(err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
+    });
   });
 
 
